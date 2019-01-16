c447945a6db23dd2cfeda7de4d9c6d248619ba57
Daniel Salas <danielsalas074@gmail.com>
2019-01-16 02:50:43 -0700

    added showPage function

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 
 const listNum = document.getElementsByTagName('li');
-const pageNum =  Math.ceil(listNum.length/10);
+const page = 1;
+
+const showPage = (list, page) => {
+    const listUp = (page * 10);
+    const listDown = listUp - 10;
+    list = list.length;
+
+    for (let i = 0; i < list; i++)
+    {
+      document.getElementsByTagName("li")[i].style.display = 'none';
+        for (let i = listDown; i < listUp; i++)
+        {
+          document.getElementsByTagName("li")[i].style.display = "";
+        }
+    }
+
+}
 
 
-
-
-//const appendPageLinks = (list) => {
+const appendPageLinks = (list) => {
+  const pageNum =  Math.ceil(listNum.length/10);
   const paginationDiv = document.createElement('div'); //assigns and creates the div element
-  let page = document.querySelector('.page'); // assigns the page to a variable
+  const page = document.querySelector('.page'); // assigns the page to a variable
   paginationDiv.className = 'pagination'; // assigns the pagination class
   const ul = document.createElement('ul'); // assigns and creates the ul tah
   page.appendChild(paginationDiv); // page appends with the paginationDiv
   paginationDiv.appendChild(ul); // paginationDiv appends the ul
 
   //for loop to create the buttons
-  for(let i = 1; i < pageNum+1; i++) // added a 1 to make the link numbers correct;
+  for(let i = 1; i < list+1; i++) // added a 1 to make the link numbers correct;
   {
     const li = document.createElement('li');
     const a = document.createElement('a');
@@ -23,10 +38,17 @@ const pageNum =  Math.ceil(listNum.length/10);
      li.appendChild(a);
      ul.appendChild(li);
   }
+  a.addEventListner('click', (e) => {
+    selectedLink = e.target.textContent;
+    let links = document.querySelector('active').className = '';
+    page = selectedLink;
+    showPage(passlist, page);
+    console.log(passlist, page);
 
-//}
+    e.target.className = 'active';
 
-//const showPage = (list, page) => {
+  })
+}
 
-//}
-//student-list
+showPage(listNum,page);
+appendPageLinks(pageNum);
